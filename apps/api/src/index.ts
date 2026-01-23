b9e7d26a8e9d2cc08f5b59c38391c19dda47faf0
shlokfreelancer <shlokporfessionalwork@gmail.com>
2026-01-23 13:56:20 +0530

    Update CORS to support FRONTEND_URL env var

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -23,7 +23,7 @@ const app = new Hono<{ Variables: Variables }>()
 
 
 app.use('/*', cors({
-    origin: ['http://localhost:3000'],
+    origin: [process.env.FRONTEND_URL || 'http://localhost:3000'],
     credentials: true
 }))
 app.use('*', logger())
